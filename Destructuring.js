52dbe8c79e3955082f2fb0fd5b1fda73783e68e2
om_pawar <ompawar3000@gmail.com>
2025-03-21 20:11:33 +0530

    js concept

---
+++ Destructuring.js
@@ -0,0 +1,20 @@
+//array  
+
+let Names = ["raj", "om", "jojo"];
+let [first, second, third] = Names;
+
+console.log(first);  
+console.log(second); 
+console.log(third);  
+
+
+console.log(); 
+
+
+//object 
+let person = { pName: "Omm", age: 21, city: "Satara" };
+let { pName, age, city } = person;
+
+console.log(pName); 
+console.log(age);  
+console.log(city); 
---
+++ jsFunctions.js
@@ -0,0 +1,14 @@
+//split function
+let text = "hello my name is om";
+let textArr = text.split(" "); 
+console.log(textArr); 
+
+// reverse function
+let numbers = [1, 2, 3, 4, 5];
+numbers.reverse();
+console.log(numbers)
+
+// join 
+let words = ["Hello", "World"];
+let sentence = words.join(" ");
+console.log(sentence); 
